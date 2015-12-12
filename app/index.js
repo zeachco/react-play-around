19038870978681ae68c9e80a75f0d24fb86013dd
Olivier <zeachco@gmail.com>
2015-12-12 15:16:53 -0500

    baseic setup react project

---
+++ app/index.js
@@ -0,0 +1,8 @@
+console.log('app started');
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render( <h1>Hello, world!</h1>,
+  document.getElementById('mainRegion')
+);
